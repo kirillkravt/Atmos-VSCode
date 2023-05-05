38355692004fbbd2e454d1ae2486f2fe1f1fdd13
kirillkravt <kirillkravt@gmail.com>
2023-05-05 23:44:21 +0300

    first commit

---
+++ js/video-list.js
@@ -0,0 +1,57 @@
+let allVideos = [
+	{
+		 name: "Melting snow",
+		 src: "./media/melting snow",
+		 id: "vid_1"
+	},
+	{
+		 name: "Moon light",
+		 src: "./media/Moon light",
+		 id: "vid_2"
+	},
+	{
+		 name: "Summer sunset",
+		 src: "./media/Summer sunset",
+		 id: "vid_3"
+	},
+	{
+		 name: "Tropic island",
+		 src: "./media/Tropic island",
+		 id: "vid_4"
+	},
+	{
+		 name: "Equalizer",
+		 src: "./media/Equalizer",
+		 id: "vid_5"
+	},
+	{
+		 name: "Fairy Forest",
+		 src: "./media/Fairy forest",
+		 id: "vid_6"
+	},
+	{
+		 name: "Сябака",
+		 src: "./media/-15305",
+		 id: "vid_7"
+	},
+	{
+		 name: "DNA Spiral",
+		 src: "./media/-6973",
+		 id: "vid_8"
+	},
+	{
+		 name: "Love you",
+		 src: "./media/-32021",
+		 id: "vid_9"
+	},
+	{
+		 name: "Hog",
+		 src: "./media/-30222",
+		 id: "vid_10"
+	},
+	{
+		 name: "lava lamp",
+		 src: "./media/-27803",
+		 id: "vid_11"
+	}
+]
